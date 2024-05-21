3dc848411179cfbfee6176d2e74f58a74b6ba7c2
Dawon-yoon <dawon3887@gmail.com>
2024-05-21 20:45:56 +0900

    modified images and css

--- src/App.js
+++ src/App.js
@@ -1,76 +1,69 @@
-import Box from './component/Box'
+import { useState } from 'react' 
 import './App.css';
-import { useState } from 'react';
+import Box from './component/Box'
 
 const choice = {
   rock: {
     name: 'Rock',
-    img: './img/rock.png'
+    img: `${process.env.PUBLIC_URL}/img/rockImg.png`
   },
-  scissor: {
+  scissors: {
     name: 'Scissors',
-    img: './img/scissors.png'
+    img:`${process.env.PUBLIC_URL}/img/scissorsImg.png`
   },
   paper: {
     name: 'Paper',
-    img: './img/fabric.png'
+    img:`${process.env.PUBLIC_URL}/img/paper.png`
   }
 }
 
 function App() {
-  const [computerSelect, setComputerSelect] = useState(null)
-  
   const [userSelect, setUserSelect] = useState(null)
-
-  const [result, setResult] = useState("")
-
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState(null)
   const [computerResult,setComputerResult] = useState(null)
   
-  const playGame = (userChoice) => {
+  const playGame = (userChoice) => { 
     setUserSelect(choice[userChoice])
-    let computerChoice = randomChoice()
-    setComputerSelect(computerChoice)
-    setResult(jedgement(choice[userChoice], computerChoice))
-    setComputerResult(computerJedgement(jedgement(choice[userChoice], computerChoice)))
+    let computerItem = getRandomItem()
+    setComputerSelect(computerItem)
+    let userResult = getResult(choice[userChoice], computerItem)
+    setResult(userResult) 
+    setComputerResult(getComputerResult(userResult))
+  }
+  const getRandomItem = () => {
+    let itemArray = Object.keys(choice)
+    let randomIndex = Math.floor(Math.random() * itemArray.length)
+    let randomItem = itemArray[randomIndex]
+    return choice[randomItem]
   }
 
-  const computerJedgement = (result) => { 
-    if (result === "Tie") {
-      return "Tie"
-    } else if (result === "Win") {
-      return "Lose"
-    } else { 
-      return "Win"
-    }
-  }
-
-  const jedgement = (user, computer) => { 
+  const getResult = (user,computer) => {
     if (user.name === computer.name) {
       return "Tie"
-    } else if (user.name === "Rock") return computer.name === "Scissors" ? "Win" : "lose"
-    else if (user.name === "Scissors") return computer.name === "Paper" ? "Win" : "Lose"
-    else if(user.name === "Paper") return computer.name === "Rock" ? "Win" : "Lose"
+    } else if (user.name === "Rock") return computer.name === "Scissors"? "Win" : "Lose";
+    else if (user.name === "Scissors") return computer.name === "Paper"? "Win" : "Lose";
+    else if (user.name === "Paper") return computer.name === "Rock"? "Win" : "Lose";
+  }
 
+  const getComputerResult = (userResult) => {
+    return userResult === "Tie" ? "Tie" : userResult === "Win"? "Lose" : "Win";
   }
-  const randomChoice = () => { 
-    let itemArray = Object.keys(choice) //
-    let randomItem = Math.floor(Math.random()*itemArray.length)
-    let final = itemArray[randomItem]
-    return choice[final]
-  }
+
   return (
-    <div>
-      <div className='main'>
-        <Box title='You' item={userSelect} result={result} />
-        <Box title='Computer' item={computerSelect} result={computerResult} />
+    <div className="wrapper">
+      <div className="gameBoxs">
+        <Box player="You" item={userSelect} result={result} />
+        <Box player="Computer" item={computerSelect} result={computerResult} />
       </div>
-      <div className='main'>
-        <button onClick={()=>playGame('scissor')}>✌️</button>
-        <button onClick={()=>playGame('rock')}>✊</button>
-        <button onClick={() => playGame('paper')}>🖐️</button>
+      <div className="btnsBox">
+        <button onClick={()=>playGame('scissors')} className="iconBtn">✌️</button>
+        <button onClick={()=>playGame('rock')} className="iconBtn">✊</button>
+        <button onClick={()=>playGame('paper')} className="iconBtn">🖐️</button>
+        <p className={`text${result}`}>{result? `${result}`:"Choose the icon!"}</p>
       </div>
-      <h2>{result? `You ${result}!`:'Choose the icon!'}</h2>
     </div>
+    
   );
 }
 
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,11 +1,9 @@
-import React from 'react'
-
-const Box = ({ title, item, result }) => {
+const Box = ({player,item,result}) => {
   return (
-      <div className={`box ${result}`}>
-          <h3 className='box-name'>{title}</h3>
-          <img className='box-img' src={item?item.img:`./img/question-mark.png`} />
-          <p className='box-result'>{result}</p>
+    <div className={`box ${result}`}>
+      <h1>{player}</h1>
+      <img src={item?item.img:`${process.env.PUBLIC_URL}/img/question-mark.png`} />
+      <p>{result}</p>
     </div>
   )
 }
